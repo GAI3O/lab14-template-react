bb72a59f112e99294b1271a0e3b863849754e018
GAI3O <bygabbo12@gmail.com>
2022-06-23 01:15:54 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Router from "./router";
+import {ThemeProvider} from "@mui/material/styles"
+import theme from "./theme"
+
+
+function App() {
+
+  return (
+    <ThemeProvider theme={theme} className="App">
+      <Router></Router>
+    </ThemeProvider>
+  )
+}
+
+export default App;
